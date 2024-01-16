6d5fb9a89a9e1c800951dfc3ed7d8f9236fa428a
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-01-15 23:27:08 -0300

    finished character building (?)

--- src/app/objects/character.ts
+++ src/app/objects/character.ts
@@ -1,2 +1,37 @@
 export class Character {
+    name: string = "";
+    title: string = "";
+    vision: string = "";
+    weapon: string = "";
+    nation: string = "";
+    affiliation: string = "";
+    rarity: number = -1;
+    constellation: string = "";
+    birthday: string = "";
+    description: string = "";
+
+    skillTalents? : SkillTalent;
+    passiveTalents? : LeveledElement;
+    constelations? : LeveledElement;
+}
+
+export interface IUnlockable {
+    name : string;
+    unlock : string;
+    description : string;
+}
+
+//Skill and passive talents, also constellations.
+export class SkillTalent implements IUnlockable {
+    name: string = "";
+    unlock: string = "";
+    description: string = "";
+    type : string = "";
+}
+
+export class LeveledElement implements IUnlockable {
+    name: string = "";
+    unlock: string = "";
+    description: string = "";
+    level : number = -1;
 }
